c8861fd839e5f45587462bd27b648967387fa9fb
Markus Meyerhofer <markus.meyerhofer@me.com>
2026-03-11 15:09:22 +0100

    removing according to gitignore

--- build/Qt_6_10_1_for_macOS-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_PizzaGenie_FILE /Users/max/Development/PizzaGenie/build/Qt_6_10_1_for_macOS-Debug/PizzaGenie.app/Contents/MacOS/PizzaGenie)
-set(__QT_DEPLOY_TARGET_PizzaGenie_TYPE EXECUTABLE)
